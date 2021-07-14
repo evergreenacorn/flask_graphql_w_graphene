e10cf27d2a6196b5063ca527b6e849394e619284
evergreenacorn <rasakjustone01.11@gmail.com>
2021-07-14 20:22:07 +0300

    Исправлены модели.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
+from app import db
 from sqlalchemy import (
-    Column, DateTime, ForeignKey, Integer, String
+    Column, DateTime, ForeignKey, Integer,
+    String, func
 )
 from sqlalchemy.orm import backref, relationship, declarative_mixin
 
@@ -7,19 +9,36 @@ from sqlalchemy.orm import backref, relationship, declarative_mixin
 # Describe your models here
 
 
-class Department:
+class Department(db.Model):
     __tablename__ = 'department'
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
 
-class Role:
+class Role(db.Model):
     __tablename__ = 'role'
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
 
-class Employee:
+class Employee(db.Model):
     __tablename__ = 'employee'
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    hired_on = Column(DateTime, default=func.now())
+    department_id = Column(Integer, ForeignKey('department.id'))
+    role_id = Column(Integer, ForeignKey('roles.role_id'))
+    department = relationship(
+        Department,
+        backref=backref(
+            'employees',
+            uselist=True,
+            cascade='delete,all')
+    )
+    role = relationship(
+        Role,
+        backref=backref(
+            'roles',
+            uselist=True,
+            cascade='delete,all')
+    )
