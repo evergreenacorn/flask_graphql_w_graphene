33c8fe1775904912615ea9fa661212862738b7cd
evergreenacorn <rasakjustone01.11@gmail.com>
2021-07-15 14:08:19 +0300

    Обновлены модели, добавлены миксины.

--- app/models.py
+++ app/models.py
@@ -1,30 +1,45 @@
-from app import db
 from sqlalchemy import (
     Column, DateTime, ForeignKey, Integer,
     String, func
 )
-from sqlalchemy.orm import backref, relationship, declarative_mixin
+from sqlalchemy.orm import (
+    backref, relationship, declarative_mixin,
+    declared_attr
+)
+from datetime import datetime
+from app import db
 
 
 # Describe your models here
-
-
-class Department(db.Model):
-    __tablename__ = 'department'
+@declarative_mixin
+class StandartModelMixin:
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
-
-class Role(db.Model):
-    __tablename__ = 'role'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
 
 
-class Employee(db.Model):
-    __tablename__ = 'employee'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+class TimestampMixin:
+    created = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow
+    )
+    updated = Column(
+        DateTime,
+        onupdate=datetime.utcnow
+    )
+
+
+class Department(StandartModelMixin, TimestampMixin, db.Model): ...
+
+
+class Role(StandartModelMixin, TimestampMixin, db.Model): ...
+
+
+class Employee(StandartModelMixin, TimestampMixin, db.Model):
     hired_on = Column(DateTime, default=func.now())
     department_id = Column(Integer, ForeignKey('department.id'))
     role_id = Column(Integer, ForeignKey('roles.role_id'))
