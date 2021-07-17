b40f210d42a187cb358c50928c757c353e24e549
evergreenacorn <rasakjustone01.11@gmail.com>
2021-07-17 20:58:24 +0300

    Добавлена настройка DEBUG.

--- config.py
+++ config.py
@@ -10,5 +10,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'app.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
