e5618a696744f76c2be0a98f5490c8b2437c4279
evergreenacorn <rasakjustone01.11@gmail.com>
2021-07-17 21:01:24 +0300

    Поправил main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from app import create_app, db
+# from app.models import Role, Department, Employee
+from app import create_app
+# from app.routes import graphql_url
 import click
 
 
 app = create_app()
+app.app_context().push()
+
+
+from app.routes import graphql_url
+graphql_url(app)
 # app.cli.run()  # https://flask.palletsprojects.com/en/2.0.x/cli/
 
 
 @app.shell_context_processor
 def make_shell_context():
-    from app.models import Role, Department, Employee
+    from app import db, Role, Department, Employee
     return {
         'app': app,
         'db': db,
